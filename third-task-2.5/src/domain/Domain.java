4ded69c0b4e742bf9e7cb4687745481f8b495331
Ryudzu <bdg-75@bk.ru>
2023-01-25 23:32:41 +0300

    third-2.5

--- third-task-2.5/src/domain/Domain.java
+++ third-task-2.5/src/domain/Domain.java
@@ -1,15 +1,20 @@
+package domain;
+
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Domain {
 
+    private static final Logger logger = Logger.getLogger(Domain.class.getName());
     public static void main(String[] args) {
 
         // Создание системы ввода с клавиатуры и вызов созданной функции compareTo согласно заданию.
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Введите список доменов: ");
+        logger.log(Level.INFO, () -> "Введите список доменов: ");
         String dom = input.nextLine();
 
         compareTo(dom);
@@ -19,31 +24,31 @@ public class Domain {
 
         // Создание массива с доменами, где разделителем на элементы выступает регулярное выражение, способное разделять строку не только по пробелам.
 
-        String[] dom_arr = dom.split("[^.[a-zA-Z]]\\s+|_|\\s|\\.\\s+|\\.$|[^.[a-zA-Z]]+$");
-        System.out.println("Cписок доменов из стандартного ввода: " + Arrays.toString(dom_arr));
+        String[] domArr = dom.split("[^.[a-zA-Z]]\\s+|_|\\s|\\.\\s+|\\.$|[^.[a-zA-Z]]+$");
+        logger.log(Level.INFO, "Cписок доменов из стандартного ввода: {0}", Arrays.toString(domArr));
 
         // Создание массива, куда будут записываться домены с обратным именем.
 
-        String[] dom_arr_reversed = new String[dom_arr.length];
+        String[] domArrReversed = new String[domArr.length];
         int i = 0;
 
         // Перебор каждого домена из dom_arr и разбиение его по точкам, и соответственно запись его элементов во временный массив.
         // После чего идет сортировка первого и последнего элемента пузырьковым методом, и запись этого обратного домена через точку с помощью
         // String.join() в массив доменов с обратным именем dom_arr_reversed.
 
-        for (String domain : dom_arr) {
-            String[] dom_arr_domain_temp = domain.split("\\.");
+        for (String domain : domArr) {
+            String[] domArrDomainTemp = domain.split("\\.");
 
-            String temp = dom_arr_domain_temp[0];
-            dom_arr_domain_temp[0] = dom_arr_domain_temp[dom_arr_domain_temp.length - 1];
-            dom_arr_domain_temp[dom_arr_domain_temp.length - 1] = temp;
+            String temp = domArrDomainTemp[0];
+            domArrDomainTemp[0] = domArrDomainTemp[domArrDomainTemp.length - 1];
+            domArrDomainTemp[domArrDomainTemp.length - 1] = temp;
 
-            dom_arr_reversed[i] = String.join(".", dom_arr_domain_temp);
+            domArrReversed[i] = String.join(".", domArrDomainTemp);
             i++;
         }
 
         // Результат выполнения программы.
 
-        System.out.println("Упорядоченный список обратных доменов: " + Arrays.toString(dom_arr_reversed));
+        logger.log(Level.INFO, "Упорядоченный список обратных доменов: {0}", Arrays.toString(domArrReversed));
     }
 }

ab8d5b7ccf590093cd67fd2d8fedebec4acb8d80
Ryudzu <bdg-75@bk.ru>
2023-01-25 21:03:04 +0300

    first-1.3

--- first-task-1.3/src/Parentheses.java
+++ first-task-1.3/src/Parentheses.java
@@ -1,33 +1,55 @@
 import java.util.*;
-
 public class Parentheses {
-
     public static void main(String[] args) {
-        // Создаем пустой стек и систему ввода с клавиатуры
+
+        // Создание пустого стека и системы ввода с клавиатуры.
+
         Stack<Character> stack = new Stack<>();
         Scanner input = new Scanner(System.in);
+
+        // Создание словаря для проверки противоположности скобок по паттерну, пример: [(]) - false; [()] - true.
+
+        Map<Character, Character> opposites = new HashMap<>();
+        opposites.put(')', '(');
+        opposites.put(']', '[');
+        opposites.put('}', '{');
 
         System.out.print("Введите строку из скобок: ");
         String parentheses = input.nextLine();
 
-        // Запускаем цикл для прохода по всем элементам строки
+        // Запуск цикла для прохода по всем элементам строки.
+
         for (int i = 0; i < parentheses.length(); i++) {
             if (parentheses.charAt(i) == '(' || parentheses.charAt(i) == '[' || parentheses.charAt(i) == '{') {
-                // Если скобка "смотрит" вправо, то добавляем ее в стэк
+
+                // Если скобка "смотрит" вправо, то идет добавление ее в стэк.
+
                 stack.push(parentheses.charAt(i));
             }
             else if (parentheses.charAt(i) == ')' || parentheses.charAt(i) == ']' || parentheses.charAt(i) == '}') {
                 try {
-                    // Если скобка "смотрит" влево, то удаляем существующую в стеке скобку, которая смотрит "вправо" из самого стека
-                    stack.pop();
+
+                    // Если последняя скобка, добавленная в стек противоположна по паттерну, то идет удаление ее из стека, иначе результат False.
+
+                    if (stack.peek().equals(opposites.get(parentheses.charAt(i)))) {
+                        stack.pop();
+                    } else {
+                        System.out.println("False");
+                        System.exit(0);
+                    }
+
                 } catch (java.util.EmptyStackException e) {
-                    // Если скобка, которая "смотрит" влево стоит в начале строки, то программа завершается с выводом False
+
+                    // Если скобка, которая "смотрит" влево стоит в начале строки, то программа завершается с выводом False.
+
                     System.out.println("False");
                     System.exit(0);
                 }
             }
         }
-        // Если стек по итогу пустой, значит скобки сбалансированны верно и результат True, иначе False
+
+        // Если стек по итогу пустой, значит скобки сбалансированны верно и результат True, иначе False.
+
         if (stack.isEmpty())
             System.out.println("True");
         else

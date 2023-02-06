3dadbbb4941f5bb37834afba5a4b74e622650d44
Ryudzu <bdg-75@bk.ru>
2023-02-07 01:07:18 +0300

    second-3.2

--- second-task-3.2/src/certification/Certification.java
+++ second-task-3.2/src/certification/Certification.java
@@ -9,8 +9,8 @@ public class Certification {
 
     static class Node {
 
-        private final int key;
-        private String name;
+        final int key;
+        String name;
 
         private Node leftChild;
         private Node rightChild;
@@ -23,66 +23,10 @@ public class Certification {
 
     private static Node root;
 
-//    public void addNode(int key, String name) {
-//        if (root == null)
-//            root = new Node(key, name);
-//        else
-//            spotToAdd(key, name, root);
-//
-//    }
-//
-//    public Node spotToAdd(int key, String name, Node currentNode) {
-//        if (currentNode == null)
-//            return new Node(key, name);
-//
-//        if (key == currentNode.key) {
-//            currentNode.name = name;
-//            return currentNode;
-//        }
-//
-//        if (key < currentNode.key)
-//            currentNode.leftChild = spotToAdd(key, name, currentNode.leftChild);
-//        else if (key > currentNode.key)
-//            currentNode.rightChild = spotToAdd(key, name, currentNode.rightChild);
-//
-//        return currentNode;
-//    }
-
     // Проверка на корневой узел.
     public boolean isBST(Node currentNode) {
         return currentNode == root;
     }
-
-//     Создание метода isBST, который будет проверять, является ли дерево бинарным.
-//     Проверка идет следующим образом: если значение дочернего элемента слева будет больше, чем значение его родителя, то вывод False,
-//     а если значение дочернего элемента справа меньше, чем значение родителя, то также False.
-//
-//    private Node currentNode = null;
-//
-//    public boolean isBST(Node root) {
-//        if (root != null) {
-//            if (!isBST(root.leftChild))
-//                return false;
-//
-//            if (currentNode != null && currentNode.key >= root.key)
-//                return false;
-//
-//            currentNode = root;
-//            return isBST(root.rightChild);
-//        }
-//        return true;
-//    }
-//
-//     Необязательный метод inOrder, который выводит все значения (ключи) в дереве по возрастанию, для понимания того
-//     как работает метод isBST.
-//
-//    public void inOrder(Node currentNode) {
-//        if (currentNode != null) {
-//            inOrder(currentNode.leftChild);
-//            logger.log(Level.INFO, " -> {0}", currentNode.key);
-//            inOrder(currentNode.rightChild);
-//        }
-//    }
 
     public static void main(String[] args) {
 

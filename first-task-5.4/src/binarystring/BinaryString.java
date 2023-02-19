6b9b63eb87b8ee5ece283375acf08857b4da380a
Ryudzu <bdg-75@bk.ru>
2023-02-19 04:20:34 +0300

    first-5.4

--- first-task-5.4/src/binarystring/BinaryString.java
+++ first-task-5.4/src/binarystring/BinaryString.java
@@ -18,7 +18,7 @@ public class BinaryString {
 
         // Результат выполнения первого случая для поиска подстрок, с 3 и более повторяющимися подряд единицами.
 
-        Pattern pt1 = Pattern.compile("((0|10)+1{3,})");
+        Pattern pt1 = Pattern.compile("((0+1{3,})|(10+1{3,}))");
         Matcher mt1 = pt1.matcher(binaryString);
         while (mt1.find()) {
             String result = String.format("a) Найдено на позиции с %d по %d: %s", mt1.start(), mt1.end(), binaryString.substring(mt1.start(), mt1.end()));
@@ -27,29 +27,27 @@ public class BinaryString {
 
         // Результат выполнения второго случая для поиска подстрок, с наличием подстроки 110.
 
-        Pattern pt2 = Pattern.compile("((0|10)*1{2,}0)");
+        Pattern pt2 = Pattern.compile("((0*1{2,}0)|(10*1{2,}0))");
         Matcher mt2 = pt2.matcher(binaryString);
 
         while (mt2.find()) {
             String result = String.format("б) Найдено на позиции с %d по %d: %s", mt2.start(), mt2.end(), binaryString.substring(mt2.start(), mt2.end()));
             logger.log(Level.INFO, "{0}\n", result);
         }
-        System.out.println();
 
         // Результат выполнения третьего случая для поиска подстрок, с наличием подстроки 1101100.
 
-        Pattern pt3 = Pattern.compile("((0|10)*1{2,}01{2}0{2})");
+        Pattern pt3 = Pattern.compile("((0*1{2,}01{2}0{2})|(10*1{2,}01{2}0{2}))");
         Matcher mt3 = pt3.matcher(binaryString);
 
         while (mt3.find()) {
             String result = String.format("в) Найдено на позиции с %d по %d: %s", mt3.start(), mt3.end(), binaryString.substring(mt3.start(), mt3.end()));
             logger.log(Level.INFO, "{0}\n", result);
         }
-        System.out.println();
 
         // Результат выполнения четвертого случая для поиска подстрок, в которые не должна входить подстрока 110.
 
-        Pattern pt4 = Pattern.compile("(((0|10)+1*)|11?)");
+        Pattern pt4 = Pattern.compile("((((10)+1*)|((01)+1*)|((0)+1*))|11?)");
         Matcher mt4 = pt4.matcher(binaryString);
 
         while (mt4.find()) {

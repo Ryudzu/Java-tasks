e9cae2970d63d5c5834a243db06b9ff03c80aefc
Ryudzu <bdg-75@bk.ru>
2023-02-20 00:04:51 +0300

    second-5.4

---
+++ second-task-5.4/src/binarystring/BinaryString.java
@@ -0,0 +1,29 @@
+package binarystring;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BinaryString {
+
+    private static final Logger logger = Logger.getLogger(BinaryString.class.getName());
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        logger.log(Level.INFO, "Введите двоичную строку: ");
+        String binaryString = input.nextLine();
+
+        // Результат выполнения программы, которая выводит строки с двумя и более непоследовательными нулями.
+
+        Pattern pt = Pattern.compile("(1*01+0(1+0?)*)");
+        Matcher mt = pt.matcher(binaryString);
+
+        while (mt.find()) {
+            String result = String.format("Найдено на позиции с %d по %d: %s", mt.start(), mt.end(), binaryString.substring(mt.start(), mt.end()));
+            logger.log(Level.INFO, "{0}\n", result);
+        }
+    }
+}
